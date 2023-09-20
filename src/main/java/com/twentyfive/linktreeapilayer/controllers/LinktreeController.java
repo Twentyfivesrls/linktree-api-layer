fc4bd9e3e5e523820e205714eece081be9b83d22
twentyfive <twentyfivesrls@gmail.com>
2023-09-20 16:18:36 +0200

    review code

--- src/main/java/com/twentyfive/linktreeapilayer/controllers/LinktreeController.java
+++ src/main/java/com/twentyfive/linktreeapilayer/controllers/LinktreeController.java
@@ -5,7 +5,6 @@ import com.twentyfive.linktreeapilayer.clients.InternalLinktreeController;
 import com.twentyfive.twentyfivemodel.models.LinkTree;
 import com.twentyfive.twentyfivemodel.models.UserLink;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class LinktreeController {
 
     @Autowired
-    private  InternalLinktreeController linktreeController;
+    private InternalLinktreeController linktreeController;
 
 
     @Autowired
@@ -23,53 +22,33 @@ public class LinktreeController {
 
     @PostMapping("/add")
     public ResponseEntity<Object> add(@RequestBody LinkTree linkTree) {
-        try{
-            String username = authenticationService.getUsername();
-            UserLink result = linktreeController.add(linkTree, username);
-            return ResponseEntity.ok().body(result);
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        String username = authenticationService.getUsername();
+        UserLink result = linktreeController.add(linkTree, username);
+        return ResponseEntity.ok().body(result);
     }
 
     @GetMapping("/findByUser")
     public ResponseEntity<Object> getByUser() {
-        try{
-            String username = authenticationService.getUsername();
-            UserLink response = linktreeController.getByUser(username);
-            return ResponseEntity.ok().body(response);
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        String username = authenticationService.getUsername();
+        UserLink response = linktreeController.getByUser(username);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/findByUsername")
     public ResponseEntity<Object> getByUsername(@RequestParam("username") String username) {
-        try{
-            return ResponseEntity.ok(linktreeController.getByUsername(username));
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        return ResponseEntity.ok(linktreeController.getByUsername(username));
     }
 
     @PutMapping("/update")
-    public ResponseEntity<Object> update(@RequestParam("id") String id, @RequestBody LinkTree linkTree){
-        try{
-            String username = authenticationService.getUsername();
-            return ResponseEntity.ok(linktreeController.update(id, username, linkTree));
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+    public ResponseEntity<Object> update(@RequestParam("id") String id, @RequestBody LinkTree linkTree) {
+        String username = authenticationService.getUsername();
+        return ResponseEntity.ok(linktreeController.update(id, username, linkTree));
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<Object> delete(@RequestParam("id") String id) {
-        try{
-            String username = authenticationService.getUsername();
-            return ResponseEntity.ok(linktreeController.delete(username,id));
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        String username = authenticationService.getUsername();
+        return ResponseEntity.ok(linktreeController.delete(username, id));
     }
 
     @RequestMapping("/test")
